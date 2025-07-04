bc7ae017b998624bccf306549bbc1a59ba1f00f4
Hansika-hub <hansika2108@gmail.com>
2025-07-04 21:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,7 @@ def process_email():
 
 if __name__ == "__main__":
     app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
